2e17b5910616f49396a67f98a3720fb2595d944c
Alek Brown <alekjbrown@live.com>
2020-11-07 19:42:15 -0600

    Base CRUD application with pages
    Includes add, edit, delete for basic video game off landing page with project name and author names

---
+++ src/main/java/VGL/repository/GameRepository.java
@@ -0,0 +1,11 @@
+package VGL.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import VGL.model.Game;
+
+@Repository
+public interface GameRepository extends JpaRepository<Game, Long> {
+
+}
