f4719eec1d43f07ad9bd911d97540310d958478a
qork13 <57408249+qork13@users.noreply.github.com>
2020-11-16 08:21:07 -0600

    Adding a Customer View page that will sort by checked out
    Added customer view html. Sorting by checked out games. Will update once
    checkout functionality is implemented.

--- src/main/java/VGL/repository/GameRepository.java
+++ src/main/java/VGL/repository/GameRepository.java
@@ -1,6 +1,6 @@
 package VGL.repository;
 
-import java.util.List;
+
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,5 +10,4 @@ import VGL.model.Game;
 @Repository
 public interface GameRepository extends JpaRepository<Game, Long> {
 	
-	List<Game> findByOrderByPlatformAsc();
 }
