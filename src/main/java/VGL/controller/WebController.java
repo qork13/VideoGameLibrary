bffaf569bd1d34f811271c89b346c85aadc01b14
qork13 <57408249+qork13@users.noreply.github.com>
2020-11-17 08:13:53 -0600

    Update Checked out page to sort by availability and Platform

--- src/main/java/VGL/controller/WebController.java
+++ src/main/java/VGL/controller/WebController.java
@@ -40,7 +40,7 @@ public class WebController {
 			return addNewGame(customer);
 		}
 		
-		customer.addAttribute("games",repo.findAll(Sort.by(Sort.Direction.ASC, "checkedOut")));
+		customer.addAttribute("games",repo.findAll(Sort.by(Sort.Direction.DESC, "checkedOut").and(Sort.by("platform"))));
 		return "customerView";
 	}
 	
@@ -69,4 +69,32 @@ public class WebController {
 		repo.delete(g);
 		return viewAllGames(model);
 	}
+	
+	@GetMapping("/checkedOut/{id}")
+	public String rentGame(@PathVariable("id") long id, Model model) {
+		Game g = repo.findById(id).orElse(null);
+		g.setCheckedOut(true);
+		repo.save(g);
+		return viewAllGames(model);
+	}
+
+	@GetMapping("/rent/{id}")
+	public String rentGame(Game g, Model model) {
+		int qty = g.getQuantity();
+		qty = qty - 1;
+		g.setQuantity(qty);
+		repo.save(g);
+		return viewAllGames(model);
+	}
+	
+	@GetMapping("/rent")
+	public String rentGame(Model model) {
+		if(repo.findAll().isEmpty()) {
+			return addNewGame(model);
+		}
+		
+		model.addAttribute("games",repo.findAll());
+		return "rent";
+	}
+
 }
